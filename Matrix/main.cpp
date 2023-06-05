3ff808a85e06257f0d38b924ce7f4fed3b9bf071
Machk_yz84@top-academy.ru <Machk_yz84@top-academy.ru>
2023-06-05 22:42:11 +0300

    MATRIX

---
+++ Matrix/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main() {
+	setlocale(LC_ALL, "");
+
+		const int COLS = 3;
+		const int ROWS = 3;
+		int MATRIX1[COLS][ROWS] = {};
+		int MATRIX2[COLS][ROWS] = {};
+		int MATRIX3[COLS][ROWS] = {};
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				MATRIX1[i][j] = rand() % 10;
+				MATRIX2[i][j] = rand() % 10;
+			}
+		}
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cout << MATRIX1[i][j] << tab;
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cout << MATRIX2[i][j] << tab;
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+		cout << "Сложение матриц: " << endl << endl;
+		//Сложение матриц
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				MATRIX3[i][j] = MATRIX1[i][j] + MATRIX2[i][j];
+			}
+		}
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cout << MATRIX3[i][j] << tab;
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+		cout << "Вычитание матриц: " << endl << endl;
+	
+		//Вычитание матриц
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				MATRIX3[i][j] = MATRIX1[i][j] - MATRIX2[i][j];
+			}
+		}
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cout << MATRIX3[i][j] << tab;
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+		cout << "Умножение матриц: " << endl << endl;
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				for (int k = j;  k < ROWS;  k++)
+				{
+					MATRIX3[i][j] = (MATRIX1[i][j] * MATRIX2[k][i]) + (MATRIX1[k][j+1] * MATRIX2[k][j+1]);
+				}
+			}
+		}
+
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cout << MATRIX3[i][j] << tab;
+			}
+			cout << endl;
+		}
+}
